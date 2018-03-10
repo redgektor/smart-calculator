2a49c1e9411afbfe513cf44a9ce86f4dd411895f
redgektor <redgektor11@gmail.com>
2018-03-10 17:54:20 +0300

    smart-calculator - Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,88 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+  this.result = 0;
+  this.str = [initialValue];
+
   }
 
   add(number) {
-    // your implementation
+    this.str.push('+');
+    this.str.push(number);
+    return this;
+
   }
   
   subtract(number) {
-    // your implementation
+    this.str.push('-');
+    this.str.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.str.push('*');
+    this.str.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.str.push('/');
+    this.str.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.str.push('^');
+    this.str.push(number);
+    return this;
+  }
+  valueOf() {
+    var array = this.str;
+    var temp = 0;
+
+    for (var i = array.length-2; i >= 1; i--) {
+      if (array[i] == '^') {
+        temp = Math.pow(+array[i-1], +array[i+1] );
+        array[i+1] = temp;
+        array.splice(i-1,2);
+      }
+    }
+
+    for (var i = array.length-2; i >= 1; i--) {
+      if (array[i] == '/') {
+        temp = +array[i-1] / +array[i+1];
+        array[i+1] = temp;
+        array.splice(i-1,2);
+      }
+    }
+
+    for (var i = array.length-2; i >= 1; i--) {
+      if (array[i] == '*') {
+        temp = +array[i-1] * +array[i+1];
+        array[i+1] = temp;
+        array.splice(i-1,2);
+      }
+    }
+    for (var i = array.length-2; i >= 0; i--) {
+      if (array[i] == '-') {
+        if (array[i-2] ==='-') {
+          temp = +array[i-1] + +array[i+1];
+        } else {
+          temp = +array[i-1] - +array[i+1];
+        }
+        array[i+1] = temp;
+        array.splice(i-1,2);
+      }
+    }
+
+    for (var i = array.length-2; i >= 0; i--) {
+      if (array[i] == '+') {
+        temp = +array[i-1] + +array[i+1];
+        array[i+1] = temp;
+        array.splice(i-1,2);
+      }
+    }
+
+    return array[0];
   }
 }
-
 module.exports = SmartCalculator;
